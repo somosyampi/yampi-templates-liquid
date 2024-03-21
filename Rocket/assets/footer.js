60db85585cc2d8dbc0b6620a2808a31d1f0f0b7f
Pedro Carvalho <equype1@gmail.com>
2024-03-21 16:32:08 -0300

    feat(FooterSection): finish footer section

---
+++ Rocket/assets/footer.js
@@ -0,0 +1,86 @@
+class FormNewsletter extends HTMLElement {
+  constructor() {
+    super();
+  }
+
+  connectedCallback() {
+    this.form = document.getElementById('form-newsletter');
+    this.form.addEventListener('submit', this.handleFormSubmit.bind(this));
+  }
+
+  async handleFormSubmit(event) {
+    event.preventDefault();
+
+    const formData = new FormData(this.form);
+
+    const url = `${window.Yampi.api_domain}/leads`;
+
+    try {
+      const response = await fetch(url, {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: formData
+      });
+
+      const data = await response.json();
+
+      if (!response.ok) {
+        throw new Error(data.message);
+      }
+
+      console.log(data);
+    } catch (error) {
+      console.error(error);
+    }
+  }
+}
+
+customElements.define('form-newsletter', FormNewsletter);
+
+class ExpandableElement extends HTMLElement {
+  static get observedAttributes() {
+    return ["expanded"];
+  }
+  
+  constructor() {
+    super();
+  }
+
+  connectedCallback() {
+    const title = this.querySelector('.title');
+    this.height = title.scrollHeight;
+    this.isExpanded = this.getAttribute('expanded') === 'true';
+    title.addEventListener('click', this.handleClick.bind(this));
+  }
+
+  attributeChangedCallback(name, oldValue, newValue) {
+    // Avoids the first time that the component is rendered, the callback is triggered but with oldValue being null
+    if (newValue && oldValue) {
+      console.log('changed');
+      console.log(name, oldValue, newValue);
+      this.isExpanded = newValue === 'true';
+    }
+  }
+
+  handleClick() {
+    console.log('click');
+    console.log(this.isExpanded);
+    if (this.isExpanded) {
+      return this.close();
+    }
+
+    return this.open();
+  }
+
+  open() {
+    this.style.height = this.scrollHeight + 'px';
+    this.setAttribute('expanded', true);
+  }
+
+  close() {
+    this.style.height = this.height + 'px';
+    this.setAttribute('expanded', false);
+  }
+}
+
+customElements.define('expandable-element', ExpandableElement);
